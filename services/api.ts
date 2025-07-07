4eed02eff70a76e98ac3a1118ba138f8261aaf86
Essam <syedessam345@gmail.com>
2025-07-07 14:56:15 +0300

    Setup API

---
+++ services/api.ts
@@ -0,0 +1,45 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`
+    }
+}
+
+
+export const fetchMovies = async ({ query }: { query: string}) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${query}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    })
+
+    if (!response.ok) {
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText)
+    }
+
+    const data = await response.json()
+
+    return data.results;
+}
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTRiNDA2MjJjMGMxYThmMjUzZmYwYWE2NThmNzIxNyIsIm5iZiI6MTc1MTc5OTAyMC4wNDksInN1YiI6IjY4NmE1NGVjZDNkOWRkNWMzZWVkNTUwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6AbBJ16qpak9dNyD9B5gGRiBCHccxZsQ7P1Hbgc37G4'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
+
+
